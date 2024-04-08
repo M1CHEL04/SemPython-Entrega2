a36f9f21858021bf80df1edd6a106b11ef4b2c06
Santiago Michel <santiagomichel2604@gmail.com>
2024-04-07 23:23:21 -0300

    realizo una funcion que resuelve el inciso 2

--- tools.py
+++ tools.py
@@ -4,4 +4,19 @@ def create_dictionary (names, goals, goals_avoided, assists):
     for n,g,g_a,a in zip(names,goals,goals_avoided,assists):
         dictionary[n]=(g,g_a,a)
     return dictionary
+
+def get_topScorer (dictionary):
+    """Esta funcion recibe un diccionario con las estadisticas de los jugadores y retorna los datos del mayor goleador"""
+    max_goals=-1
+    max_name=None
+    max_ga=None
+    max_a=None
+    for name, (g,g_a,a) in dictionary.items():
+        if max_goals < g:
+            max_goals=g
+            max_name=name
+            max_ga=g_a
+            max_a=a
     
+    return max_name,max_goals,max_ga,max_a
+        
